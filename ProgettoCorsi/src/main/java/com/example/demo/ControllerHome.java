932451245f7d8a39ee4601ac1f21510d7207259c
Stefano-Toneatto <80056463+Stefano-Toneatto@users.noreply.github.com>
2022-06-30 15:51:53 +0200

    controller

---
+++ ProgettoCorsi/src/main/java/com/example/demo/ControllerHome.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerHome {
+
+	@RequestMapping("/")
+	public String homepage() {
+		return "home/Homepage";
+	}
+	
+	@RequestMapping("/redirector")
+	public String redirector() {
+		if(true) {
+			return "Admin/HomeAdmin";
+		}
+		else {
+			return "Utenti/HomeUtenti";
+		}
+	}
+	
+}
