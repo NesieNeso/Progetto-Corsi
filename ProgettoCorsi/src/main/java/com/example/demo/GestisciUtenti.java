3ec8ae82673606d5e24a8c5350d4d30dc5714808
andrea <andrearecchia92@outlook.it>
2022-06-30 17:07:36 +0200

    rimosso test()

--- ProgettoCorsi/src/main/java/com/example/demo/GestisciUtenti.java
+++ ProgettoCorsi/src/main/java/com/example/demo/GestisciUtenti.java
@@ -12,7 +12,6 @@ public class GestisciUtenti {
 	
 	public GestisciUtenti(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate=jdbcTemplate;
-		test();
 	}
 	
 	public int insert(String username, String nome, String cognome, String password, String email) {
@@ -25,14 +24,6 @@ public class GestisciUtenti {
 		List<String> idList = new ArrayList<>();
 		idList.addAll(jdbcTemplate.queryForList("SELECT idUtente from utenti where username='" + username + "' and password='" + password+"'", String.class));
 		return idList;		
-	}
-	
-	public void test() {
-		System.out.println("Prova inserimento:");
-		insert("LB", "Luigi", "Bianchi", "lbpass", "lb@outlook.it");
-		System.out.println("Prova ricerca id:");		
-		String list = String.join("\n", getIdFromUserPassword("JG","password"));
-		System.out.println(list);
 	}
 	
 }
