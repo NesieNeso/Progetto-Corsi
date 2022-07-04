46e13b895ee0dfb1a40e97257ef8ae8d5fd5862d
matteo <matteo-del@hotmail.it>
2022-07-04 17:57:05 +0200

    grafica

--- ProgettoCorsi/src/main/java/com/example/demo/ProgettoCorsiApplication.java
+++ ProgettoCorsi/src/main/java/com/example/demo/ProgettoCorsiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import gestioneConnessione.Connessione;
 
 @SpringBootApplication
 public class ProgettoCorsiApplication {
@@ -11,10 +14,16 @@ public class ProgettoCorsiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoCorsiApplication.class, args);
 		
+		/*
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		Connessione cn = (Connessione)context.getBean("dataSource");
+		
+		System.out.println(cn.getPassword());
 		
 		
-		//ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-		
+		JdbcTemplate cb = cn.getTemp();
+		*/
 	}
 
 }
